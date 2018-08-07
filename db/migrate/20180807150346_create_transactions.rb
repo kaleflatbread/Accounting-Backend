77404546cbad75d35196f8944c85169eeb824772
Kayla Fitzgerald <fitzgerald.km11@gmail.com>
2018-08-07 15:36:14 -0400

    fixed transaction schema

--- db/migrate/20180807150346_create_transactions.rb
+++ db/migrate/20180807150346_create_transactions.rb
@@ -2,6 +2,7 @@ class CreateTransactions < ActiveRecord::Migration[5.2]
   def change
     create_table :transactions do |t|
       t.integer :amount
+      t.string :transaction_type
       t.integer :account_id
       t.integer :user_id
       t.string :memo
