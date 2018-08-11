8fa8fdf25cd53fdfa7a7a7c8ad17c1f15e0e08fa
Kayla Fitzgerald <fitzgerald.km11@gmail.com>
2018-08-11 18:55:58 -0400

    fixed user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,11 @@ cash = Account.create({name: "Cash", number: "1000", parent_id: balance_sheet.id
 finished_goods = Account.create({name: "Finished Goods", number: "1100", parent_id: balance_sheet.id})
 
 
-kayla = User.create({email: "test1@gmail.com", first_name: "Kayla", last_name: "Fitzgerald", role: "Staff Accountant"})
-vicki = User.create({email: "test2@gmail.com", first_name: "Vicki", last_name: "Zhang", role: "Controller"})
-madison = User.create({email: "test3@gmail.com", first_name: "Madison", last_name: "O'Connor", role: "Accounting Manager"})
-jing = User.create({email: "test4@gmail.com", first_name: "Jing", last_name: "Bian", role: "Senior Accountant"})
+kayla = User.create({email: "test1@gmail.com", first_name: "Kayla", last_name: "Fitzgerald", role: "Staff Accountant", password: "password"})
+vicki = User.create({email: "test2@gmail.com", first_name: "Vicki", last_name: "Zhang", role: "Controller", password: "password"})
+madison = User.create({email: "test3@gmail.com", first_name: "Madison", last_name: "O'Connor", role: "Accounting Manager", password: "password"})
+jing = User.create({email: "test4@gmail.com", first_name: "Jing", last_name: "Bian", role: "Senior Accountant", password: "password"})
+dobby = User.create({email: "dobby@gmail.com", first_name: "Dobby", last_name: "Fitzgerald", role: "Cutest one in the office", password: "password"})
 
 
 one = Expense.create({account_id: revenue.id, transaction_type: "Journal Entry", user_id: madison.id, memo: "July 2018 revenue", date: Date.strptime("7/31/2018", "%m/%d/%Y"), amount: 500})
